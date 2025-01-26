8a7a0a50d91dc19a6f6fa39bc128404c696797da
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-01-26 17:38:28 +0100

    Update for v0.2.0

--- src/main.js
+++ src/main.js
@@ -9,9 +9,11 @@ import '../assets/images/favicon.svg';
 
 NavbarImage.src = toolbarLogo;
 
+let tactilityVersion = 'v0.2.0'
+
 function setManifest(device) {
     const installer = document.querySelector('esp-web-install-button');
-    installer.manifest = `rom/0.1.0/${device}.json`
+    installer.manifest = `rom/` + tactilityVersion + `/${device}.json`
     console.log("Device updated")
 }
 
@@ -19,17 +21,51 @@ function updateSelectionBoxVisibility() {
     const deviceSelect = $('#device');
     const installActualButton = $('#installActualButton');
     const selectedDevice = deviceSelect[0].value;
+
+    $('#TactilityVersion').html(tactilityVersion);
+
     console.log(`Selected Device: ${selectedDevice}`);
-    if (selectedDevice != "null"){
+    if (selectedDevice !== "null"){
         setManifest(selectedDevice);
         installActualButton.removeClass('invisible');
-        
-        if (selectedDevice == 'lilygo-tdeck') {
-            $('#DeviceInfo').removeClass('invisible');
-            $('#DeviceInfo').html('Press the trackball and then the reset button at the same time, then let go of the reset button, then the trackball.<br/>\
-              When this website reports that flashing is finished, you likely have to press the reset button.');
+
+        let deviceInfo = $('#DeviceInfo');
+        if (selectedDevice === 'lilygo-tdeck') {
+            deviceInfo.removeClass('invisible');
+            deviceInfo.html(
+                'If two serial devices are visible, try them both.<br/><br/>' +
+                'To put the device into bootloader mode: <br/>' +
+                '1. Press the trackball and then the reset button at the same time,<br/>' +
+                '2. Let go of the reset button, then the trackball.<br/><br/>' +
+                'When this website reports that flashing is finished, you likely have to press the reset button.'
+            );
+        } else if (selectedDevice === 'unphone') {
+            deviceInfo.removeClass('invisible');
+            deviceInfo.html(
+                '⚠️There is a power drain issue that slowly depletes the device when it\'s off. It lasts about 3 days. ⚠<br/>' +
+                '⚠️Completely depleting a battery can permanently decrease capacity. ⚠<br/><br/>' +
+                'This is a newly implemented device, so there might be other issues. Use at your own risk.<br/><br/>' +
+                'Put the device into bootloader mode by pressing the center nav button and reset for 2-3 seconds, then release reset, then release the nav button.<br/>' +
+                'After flashing is finished, press the reset button to reboot.'
+            );
+        } else if (selectedDevice === 'm5stack-core2') {
+            deviceInfo.removeClass('invisible');
+            deviceInfo.html('If you have trouble connecting to the device:<br/>' +
+                'You need to see the "USB single serial" device appear.<br/>' +
+                '1. Connect the USB cable and wait for device to boot.<br/>' +
+                '2. Press the reset button and wait for reboot.<br/>' +
+                '3. Refresh this webpage and try flashing again.'
+            );
+        } else if (selectedDevice === 'm5stack-cores3') {
+            deviceInfo.removeClass('invisible');
+            deviceInfo.html('If you have trouble connecting to the device:<br/>' +
+                'There might be 2 serial devices with the same name. Try both.<br/>' +
+                '1. Connect the USB cable and wait for device to boot.<br/>' +
+                '2. Press the reset button and wait for reboot.<br/>' +
+                '3. Refresh this webpage and try flashing again.'
+            );
         } else {
-            $('#DeviceInfo').addClass('invisible');
+            deviceInfo.addClass('invisible');
         }
         
     } else {
